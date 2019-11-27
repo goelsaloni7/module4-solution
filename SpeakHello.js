8a96d06e28cf58fc2b402443b59f0d770bc82c12
Saloni <goelsaloni7@gmail.com>
2019-11-27 22:37:25 +0530

    correct script

--- SpeakHello.js
+++ SpeakHello.js
@@ -22,3 +22,16 @@ function speak(name) {
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 // xxxx.xxxx = helloSpeaker;
+
+
+(function (window) {
+  var helloSpeaker = {};
+
+  var speakWord = "Hello ";
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + name);
+  }
+
+  window.helloSpeaker = helloSpeaker;
+
+})(window);
